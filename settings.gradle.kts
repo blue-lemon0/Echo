4286226c079e55e533235637bfdebec3d9ab3007
huaiqi <>
2026-06-04 10:18:36 +0800

    新增几个依赖镜像站

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 pluginManagement {
     repositories {
+        // 国内镜像加速 - 阿里云 (优先级最高)
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +18,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 国内镜像加速 - 阿里云 (优先级最高)
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
     }
